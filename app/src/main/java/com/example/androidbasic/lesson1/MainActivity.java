121cd3ad2ad85d1334f5d4b6204eeab06e7c1ddb
Alex <a_v_ilinskiy@mail.ru>
2021-03-17 20:49:44 +0500

    Created new package, moving 1st package's files

--- app/src/main/java/com/example/androidbasic/lesson1/MainActivity.java
+++ app/src/main/java/com/example/androidbasic/lesson1/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.androidbasic;
+package com.example.androidbasic.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.androidbasic.R;
 
 public class MainActivity extends AppCompatActivity {
 
